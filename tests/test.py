1c6ff7ab5879d2e2817a6fc367b01ef50aba14d4
runye.wang@emory.edu <runye.wang@emory.edu>
2018-07-19 15:46:17 -0400

    fix logger for local vars and fix gui uptil comparison

--- tests/test.py
+++ tests/test.py
@@ -1,25 +1,24 @@
 # -*- coding: utf-8 -*-
-import svo_extraction
 import unittest
 import sys
 import os
 
 # insert the src dir
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import svo_extraction
 
 
 class BasicTestSuite(unittest.TestCase):
     """Basic test cases."""
 
     def test_absolute_truth_and_meaning(self):
-        self.assertIsNone(svo_extraction.Corpus())
+        corpus = svo_extraction.Corpus(file_path='./sample/Murphy.txt',
+                                       output_dir='./sample/out')
+        corpus.set_up()
+        corpus.clean_up()
+        coref = svo_extraction.Coref(corpus)
+        coref.display('neural_coref')
 
 
 if __name__ == '__main__':
-    corpus = svo_extraction.Corpus()
-    corpus.set_up()
-    corpus.clean_up()
-    print(corpus)
-    coref = svo_extraction.Coref(corpus)
-    print(coref)
-    coref.display('helpers.neural_coref')
+    test = BasicTestSuite().test_absolute_truth_and_meaning()
